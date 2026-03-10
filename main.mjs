6f6f89018f24e9de8e51e1131e2241ae587f99e7
Alena Kurepina <alenarepina96@gmail.com>
2026-03-10 13:33:56 +0100

    Initial commit

---
+++ main.mjs
@@ -0,0 +1,49 @@
+const baseURL = "https://alchemy-kd0l.onrender.com";
+const startURL = `${baseURL}/start`;
+const statusURL = `${baseURL}/status`;
+const submitURL = `${baseURL}/submit`;
+const clueURL = `${baseURL}/clue`;
+
+let token = null;
+
+const userConfig = {
+    "email": "alenak@uia.no",
+    "nick": "KGB agent", "pin": "4321"
+};
+
+let respons = await (await fetch(startURL, {
+    body: JSON.stringify(userConfig),
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    }
+})).json();
+
+token = respons.token;
+
+console.log(token);
+console.log(startURL);
+
+respons = await (await fetch(statusURL, {
+    method: "GET",
+    headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": token,
+    }
+})).json();
+
+console.log(respons);
+
+respons = await (await fetch(submitURL, {
+    method: "POST",
+    headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": token,
+    },
+    body: JSON.stringify({"answer": "4"}) 
+})).json();
+
+console.log(respons);
+
